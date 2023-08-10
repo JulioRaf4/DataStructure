8c8adaf0f7ff7f07b73de0ccd8af21c8bc5ca00a
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2023-08-10 23:05:30 +0000

    Update

---
+++ Primeira unidade/Primeiro Exemplo/classes/locadora.py
@@ -0,0 +1,27 @@
+
+class Locadora:
+    def __init__(self):
+        self.indiceD = 0
+        self.indiceF = 0
+        self.array_disco = []
+        self.array_filme = []
+    
+    def novoDisco(self, disco):
+        if len(self.array_disco) <= 2:
+            self.array_disco.append(disco)
+        else:
+            print("Número excedido!")
+        
+    def novoFilme(self, Filme):
+        if len(self.array_disco) <= 2:
+            self.array_filme.append(Filme)
+        else:
+            print("Número excedido!")
+            
+    def listarDiscos(self):
+        for x in self.array_disco:
+            x.lista_informacoes()
+    
+    def listarFilme(self):
+        for x in self.array_filme:
+            x.lista_informacoes()
