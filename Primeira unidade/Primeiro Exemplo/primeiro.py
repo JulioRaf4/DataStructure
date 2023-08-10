eb2dd3f568e6c1b588a6e79951b6ba8910924511
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2023-08-09 23:35:54 -0300

    Update 1º

---
+++ Primeira unidade/Primeiro Exemplo/primeiro.py
@@ -0,0 +1,36 @@
+from abc import ABC, abastractmethod
+
+
+# Como o python não tem algo como 
+# > private String titulo; < 
+# Usei __titulo como um bom dev python
+
+class Item:
+    def __ini__(self, titulo, ano_lancamento, comentario):
+        self.__titulo = titulo
+        self.__ano_lancamento = ano_lancamento
+        self.__comentario = comentario
+
+    def get_titulo(self):
+        return self.__titulo
+
+    def set_titulo(self, titulo):
+        self.__titulo = titulo
+
+    def get_ano_lancamento(self):
+        return self.ano_lancamento
+
+    def set_ano_lancamento(self, ano_lancamento):
+        self.__ano_lancamento = ano_lancamento
+    
+    def get_comentario(self):
+        return self.__comentario
+
+    def set_comentario(self, comentario):
+        self.__comentario = comentario
+
+    # Usei o decorator @abstractmethod
+    # Para intanciar o metodo abastrato 
+    @abstractmethod
+    def lista_informacoes(self):
+        pass
