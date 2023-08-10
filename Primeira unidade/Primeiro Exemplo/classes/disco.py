883c69b50bbf601d647a14972d8c44a90f24c3f2
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2023-08-10 19:00:07 +0000

    Update

---
+++ Primeira unidade/Primeiro Exemplo/classes/disco.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+import item
+
+
+class Disco(Item):
+    def __init__(self, titulo, ano_lancamento, comentario, nome_artista, quantidade_faixas):
+        super().__init__(titulo, ano_lancamento, comentario)
+        self.nome_artista = nome_artista
+        self.quantidade_faixas = quantidade_faixas
+        self.midia = ""  # Contrutor padrão
+
+    def get_nome_artista(self):
+        return self.nome_artista
+
+    def set_nome_artista(self, nome_artista):
+        self.nome_artista = nome_artista
+
+    def get_quantidade_faixas(self):
+        return self.quantidade_faixas
+
+    def set_quantidade_faixas(self, quantidade_faixas):
+        self.quantidade_faixas = quantidade_faixas
+
+    def get_midia(self):
+        return self.midia
+
+    def set_midia(self, midia):
+        self.midia = midia
+    
+    @abstractmethod
+    def lista_informacoes(self):
+        pass
