b3dc8a5d23628a59ae0c75a037c6f1969188b489
Julio Rafael <127954141+JulioColabDev@users.noreply.github.com>
2023-09-09 22:51:57 -0300

    Add cpp

--- Primeira unidade/doublelinked list/doublelinkedlist.cpp
+++ Primeira unidade/doublelinked list/doublelinkedlist.cpp
@@ -1,16 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int value;
-    Node* next;
-    Node* previous;
+    Node *next;
+    Node *previous;
 };
 
-int main()
-{
-    Node* node = new Node();    
+void printForward(Node*head) {
+    Node* traverse = head;
+    while(traverse != nullptr) {
+        cout << traverse->value<<endl;
+        traverse = traverse->next;
+    }
+}
+
+void printBackward(Node*tail) {
+    Node* traverse = tail;
+    while(traverse != nullptr) {
+        cout << traverse->value<<endl;
+        traverse = traverse->previous;
+    }
+}
+
+int main() {
+    Node *head;
+    Node *tail;
+
+    Node *node = new Node();
+    node->value = 4;
+    node->next = nullptr;
+    node->previous = nullptr;
+    head = node;
+    tail = node;
+
+    node = new Node();
+    node->value = 5;
+    node->next = nullptr;
+    node->previous = tail;
+    tail->next = node;
+    tail = node;
+    
+    node = new Node();
+    node->value = 6;
+    node->next = nullptr;
+    node->previous = tail;
+    tail->next = node;
+    tail = node;
+    
+    node = new Node();
+    node->value = 7;
+    node->next = nullptr;
+    node->previous = tail;
+    tail->next = node;
+    tail = node;
+
+    printBackward(tail);
+
     cin.get();
-    printf("%d", node->value);
 }
