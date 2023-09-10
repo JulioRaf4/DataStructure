62d36b9760039a33a1a696a0360a6ad267dc2e71
Julio Rafael <juliorafaelnft@hotmail.com>
2023-09-10 19:19:28 -0300

    Update

--- Primeira unidade/doublelinked list/doublelinkedlist.cpp
+++ Primeira unidade/doublelinked list/doublelinkedlist.cpp
@@ -1,62 +1,91 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
+class Node {
+
 public:
     int value;
     Node *next;
     Node *previous;
 };
 
-void printForward(Node *head)
-{
+void printForward(Node *head) {
     Node *traverse = head;
-    while (traverse != nullptr)
-    {
+    while (traverse != nullptr) {
+    
         cout << traverse->value << endl;
         traverse = traverse->next;
     }
 }
- //test
-void printBackward(Node *tail)
-{
+// test
+void printBackward(Node *tail) {
     Node *traverse = tail;
-    while (traverse != nullptr)
-    {
+    while (traverse != nullptr) {
+    
         cout << traverse->value << endl;
         traverse = traverse->previous;
     }
 }
 
-void insertAtBeginning(Node *&head, int value)
-{
+void insertAtBeginning(Node *&head, int value) {
     Node *newNode = new Node;
     newNode->value = value;
     newNode->next = head;
     newNode->previous = nullptr;
-    if (head != nullptr)
-    {
+    if (head != nullptr) {
         head->previous = newNode;
     }
     head = newNode;
 }
 
-void insertAtEnd(Node *&tail, int value)
-{
+void insertAtEnd(Node *&tail, int value) {
+    
     Node *newNode = new Node;
     newNode->value = value;
     newNode->next = nullptr;
     newNode->previous = tail;
-    if (tail != nullptr)
-    {
+    if (tail != nullptr) {
         tail->next = newNode;
     }
     tail = newNode;
 }
 
-int main()
-{
+void removeNode(Node *&head, Node *&tail, int value) {
+
+    Node *current = head;
+    while (current != nullptr) {
+        if (current->value == value) {
+            if (current->previous != nullptr) {
+                current->previous->next = current->next;
+            }
+            else {
+                head = current->next;
+            }
+            if (current->next != nullptr) {   
+                current->next->previous = current->previous;
+            }
+            else {
+            
+                tail = current->previous;
+            }
+            delete current;
+            return;
+        }
+        current = current->next;
+    }
+}
+
+void clearList(Node *head, Node *tail) {
+    while (head != nullptr) {
+        Node *current = head;
+        head = current->next;
+        delete current;
+    }
+    tail = nullptr;
+}
+
+int main() {
+
     Node *head;
     Node *tail;
 
@@ -80,7 +109,7 @@ int main()
     node->previous = tail;
     tail->next = node;
     tail = node;
-    
+
     node = new Node();
     node->value = 7;
     node->next = nullptr;
@@ -91,5 +120,6 @@ int main()
     insertAtBeginning(head, 99);
     insertAtEnd(tail, 55);
     printForward(head);
+    getSize(node);
     cin.get();
 }
