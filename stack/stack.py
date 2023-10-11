1cdce8e552031870b6e274022f7aebb8c0838cac
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2023-10-11 01:43:43 +0000

    Update

---
+++ stack/stack.py
@@ -0,0 +1,26 @@
+class x:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+
+class stack:
+    def __init__(self):
+        self.size = 0
+        self.top = None
+    
+    def top():
+        pass
+    
+    def push():
+        pass
+    
+    def pop():
+        pass
+    
+    def pull():
+        pass
+
+
+stack = stack()
+
+print(stack)
