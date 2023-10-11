c7f57872d43634feb46e0fddf13a69a5439453d1
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2023-10-11 02:18:15 +0000

    Update

--- stack/stack.py
+++ stack/stack.py
@@ -1,26 +1,38 @@
-class x:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-
-class stack:
+class Node:
+    def __init__(self, *args):
+        self.data = args
+        self.next = None
+        
+class Stack:
     def __init__(self):
-        self.size = 0
+        self._size = 0
         self.top = None
+
+    def pull_on_top(self, data):
+        new_node = Node(data)
+        if self.top is None:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self._size += 1
+
+    def top_(self):
+        if self.top: return self.top.data
+        else: raise IndexError("Lista vazia")
     
-    def top():
+    def push(self):
         pass
     
-    def push():
+    def pop(self):
         pass
     
-    def pop():
-        pass
-    
-    def pull():
-        pass
+    def __size__(self):
+        return self._size
 
 
-stack = stack()
-
-print(stack)
+stack = Stack()
+stack.pull_on_top(10)
+stack.pull_on_top(23)
+stack.pull_on_top(40)
+print(stack.top_())
